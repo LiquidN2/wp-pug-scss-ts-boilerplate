ee53741401897e647e8776944925f7a730c459e9
Hugh Nguyen <hhnguyen255@gmail.com>
2022-10-06 09:28:50 +0700

    initial commit

---
+++ src/ts/script.ts
@@ -0,0 +1,8 @@
+// ------------------------------
+import '../scss/style.scss';
+
+const greet = (name: string) => {
+  console.log(`Hello ${name}`);
+};
+
+greet('Hugh');
---
+++ webpack.prod.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+
+  devtool: 'source-map',
+
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
+
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/views/pages/index.html'),
+      filename: 'index.html',
+      inject: 'head',
+      scriptLoading: 'defer',
+    }),
+  ],
+
+  module: {
+    rules: [
+      // Load TS
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+
+      // Load Pug template to JS
+      { test: /\.pug$/, loader: 'pug-loader' },
+
+      // Load CSS, SASS, SCSS
+      {
+        test: /\.s?css$/i,
+        use: [
+          // Extract CSS from JS
+          MiniCssExtractPlugin.loader,
+
+          // Translates CSS into CommonJS
+          'css-loader',
+
+          // Add PostCSS Autoprefixer (see postcss.config.js)
+          'postcss-loader',
+
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+});
